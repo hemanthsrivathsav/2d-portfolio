b9c50ca9c3700be31ee335d654e8032d9f39f8f8
Hemanth Srivathsav <hemanthsrivathsav@gmail.com>
2024-11-15 17:11:06 +0530

    changed pc msg

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,8 @@
 export const scaleFactor = 4 ;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly In Python, SQL, JavaScript, React these days.
-      Regardless of the language, I like programming.
-      Here is my <a href="https://github.com/hemanthsrivathsav" target="_blank">Github</a>!`,
+    projects: `This is my PC my workstation of Ideas. I work mostly In Python, SQL, JavaScript, React these days.
+      Regardless of the language, I like programming. Here is my <a href="https://github.com/hemanthsrivathsav" target="_blank">Github</a>!`,
 
     "cs-degree": `This is my BTech CSE degree.\nI'm proud of it. I think it gave me a good foundation.`,
 
@@ -20,8 +19,6 @@ export const dialogueData = {
     Contact me at hemanthsrivathsav@gmail.com \n `,
 
     certs:`Here are my Achievements : Here is my TensorFlow Developer Certificate: <a href="https://www.credential.net/27880ce5-bd18-47db-9245-2f9f98de7ccb#gs.6s2fyz#acc.IU6mh5Qu" target="_blank">TensorFlow Developer</a> `,
-
-    projects: ` This is my PC my workstation of Ideas, Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.`,
 
     library: ` I read a lot of books. I recently read The Golden Road by William Dalrymple.
     I probably read only some of them. Who else compulsively buys books without ever finishing them? XD`,
