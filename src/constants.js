2db528440d5819e151ab44bcd68595bb77c8d9a3
Hemanth Srivathsav <hemanthsrivathsav@gmail.com>
2024-11-15 13:57:55 +0530

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,29 @@
+export const scaleFactor = 4 ;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly In Python, SQL, JavaScript, React these days.
+      Regardless of the language, I like programming.
+      Here is my <a href="https://github.com/hemanthsrivathsav" target="_blank">Github</a>!`,
+
+    "cs-degree": `This is my BTech CSE degree.\nI'm proud of it. I think it gave me a good foundation.`,
+
+
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube & play Games. 
+    I learn a lot from interenet that's how I built this portfolio you're currently playing through!`,
+
+    tv:` I like to watch Anime and Scifi Movies a Lot !!!`,
+
+    bed: `This where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I forget about it.`,
+
+    resume: `Open my Resume. <a href="https://my.spline.design/untitled-0741ae787374a9c52a8ebcec7f96d867/" target="_blank"> Check it out </a>
+    Contact me at hemanthsrivathsav@gmail.com `,
+
+    certs:`Here are my Achievements : Here is my TensorFlow Developer Certificate: <a href="https://www.credential.net/27880ce5-bd18-47db-9245-2f9f98de7ccb#gs.6s2fyz#acc.IU6mh5Qu" target="_blank">TensorFlow Developer</a> `,
+
+    projects: ` This is my PC my workstation of Ideas, Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.`,
+
+    library: ` I read a lot of books. I recently read The Golden Road by William Dalrymple.
+    I probably read only some of them. Who else compulsively buys books without ever finishing them? XD`,
+
+    exit: `If you want to exit My portfolio, just close the tab.`,
+  };
